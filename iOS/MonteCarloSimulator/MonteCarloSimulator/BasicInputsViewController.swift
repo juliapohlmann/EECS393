11b03a43a0b2331b054763b62460340de041b839
juliapohlmann <julia.pohlmann@case.edu>
2016-10-23 10:52:43 -0400

    added all the views and navigation controllers
    Signed-off-by: juliapohlmann <julia.pohlmann@case.edu>

---
+++ iOS/MonteCarloSimulator/MonteCarloSimulator/BasicInputsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BasicInputsViewController.swift
+//  MonteCarloSimulator
+//
+//  Created by Julia Pohlmann on 10/22/16.
+//  Copyright © 2016 EECS393. All rights reserved.
+//
+
+import UIKit
+
+class BasicInputsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if(sender === )
+//    }
+    
+    
+}
+
