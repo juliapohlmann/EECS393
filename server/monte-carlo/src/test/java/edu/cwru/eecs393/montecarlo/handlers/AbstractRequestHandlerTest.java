e3327f22c42ee698287db736313e7dc331eaf298
David Bauer <dmb172@case.edu>
2016-10-25 13:06:23 -0400

    Fixed error with Jackson type discovery.

--- server/monte-carlo/src/test/java/edu/cwru/eecs393/montecarlo/handlers/AbstractRequestHandlerTest.java
+++ server/monte-carlo/src/test/java/edu/cwru/eecs393/montecarlo/handlers/AbstractRequestHandlerTest.java
@@ -3,10 +3,15 @@
  */
 package edu.cwru.eecs393.montecarlo.handlers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import junit.framework.TestCase;
 import lombok.Data;
@@ -18,21 +23,25 @@ import lombok.Data;
 public class AbstractRequestHandlerTest extends TestCase {
 
 	@Test
-	public void testDataToJson() {
+	public void testDataToJson() throws JsonParseException, JsonMappingException, IOException {
 		List<String> list = new ArrayList<>();
 		list.add("a");
 		list.add("b");
-		TestData data = new TestData("name", 1, list);
+		TestData data = new TestData();
+		data.setName("name");
+		data.setNum(1);
+		data.setList(list);
 		String json = AbstractRequestHandler.dataToJson(data);
-		String expected = "{\r\n  \"name\" : \"name\",\r\n  \"num\" : 1,\r\n  \"list\" : [ \"a\", \"b\" ]\r\n}";
-		assertEquals(expected, json);
+		ObjectMapper mapper = new ObjectMapper();
+		TestData readVersion = mapper.readValue(json, TestData.class);
+		assertEquals(data, readVersion);
 	}
 
 	@Data
-	private class TestData {
-		private final String name;
-		private final int num;
-		private final List<String> list;
+	public static class TestData {
+		private String name;
+		private int num;
+		private List<String> list;
 	}
 
 }
