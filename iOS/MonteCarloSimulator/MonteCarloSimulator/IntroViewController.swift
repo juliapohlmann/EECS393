1760e2742b799bb4e4b50fa51066351f4b3a8ac0
shyamkotak <me@shyamkotak.com>
2016-10-26 15:42:40 -0400

    test for intro screen

--- iOS/MonteCarloSimulator/MonteCarloSimulator/IntroViewController.swift
+++ iOS/MonteCarloSimulator/MonteCarloSimulator/IntroViewController.swift
@@ -12,7 +12,6 @@ class IntroViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
