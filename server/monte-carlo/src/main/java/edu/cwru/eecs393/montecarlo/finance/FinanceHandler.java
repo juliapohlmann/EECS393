2c6bbacf5c14b586a0fc63c7bf6eb164fd2490ae
David Bauer <dmb172@case.edu>
2016-10-27 16:38:26 -0400

    Added Yahoo! Finance URL creator and tests.

--- server/monte-carlo/src/main/java/edu/cwru/eecs393/montecarlo/finance/FinanceHandler.java
+++ server/monte-carlo/src/main/java/edu/cwru/eecs393/montecarlo/finance/FinanceHandler.java
@@ -5,19 +5,20 @@ import java.util.Map;
 
 /**
  * Interface for retrieving current financial market data based.
- * 
+ *
  * @author David
  *
  */
 public interface FinanceHandler {
 
 	/**
-	 * Retrieves the ticker prices for a list of stocks.
-	 * 
+	 * Retrieves the ticker prices, and historical prices for a list of stocks.
+	 *
 	 * @param tickers
 	 *            a list containing the tickers for every desired stock
-	 * @return a Map, mapping a ticker to it's price.
+	 * @return a Map, mapping a ticker to a list of prices, the first being most
+	 *         recent with each subsuquent price moving one year back in time
 	 */
-	Map<String, Double> getTickerPrices(List<String> tickers);
+	Map<String, List<Double>> getTickerPrices(List<String> tickers);
 
 }
