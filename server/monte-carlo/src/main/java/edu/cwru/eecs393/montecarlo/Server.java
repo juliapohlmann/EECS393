931f8cc707df974d13cf6f768896f65bbe392dfe
David Bauer <dmb172@case.edu>
2016-10-27 13:58:51 -0400

    Added log configuration.

--- server/monte-carlo/src/main/java/edu/cwru/eecs393/montecarlo/Server.java
+++ server/monte-carlo/src/main/java/edu/cwru/eecs393/montecarlo/Server.java
@@ -3,7 +3,11 @@ package edu.cwru.eecs393.montecarlo;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 
 import edu.cwru.eecs393.montecarlo.handlers.RequestHandler;
 import edu.cwru.eecs393.montecarlo.handlers.SimulationRequestHandler;
@@ -20,7 +24,8 @@ public class Server {
 
 	private static final RequestHandler simulationHandler = new SimulationRequestHandler();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SecurityException, FileNotFoundException, IOException {
+		LogManager.getLogManager().readConfiguration(new FileInputStream("logs/logging.properties"));
 		log.log(Level.INFO, "Initializing routes.");
 		get("/hello", (req, res) -> "Hello World");
 		post("/simulation", (req, res) -> simulationHandler.handleRequest(req, res));
